bf522974d1b13e3bb66cd29316e1d2c3c0a56f6c
Dipika Sengupta <senguptadipika30@gmail.com>
2024-05-13 13:41:46 +0200

    Update __init__.py
    Update __init__.py to change the postgres uri to be accepted from RDS postgres

--- talkintunes/__init__.py
+++ talkintunes/__init__.py
@@ -4,11 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-DATABASE_URI = os.getenv("DATABASE_URI")
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
@@ -22,7 +18,6 @@ mail = Mail()
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
-    # app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
